bb5cd7bab0d5408b150e5be1f27ccadff514438e
emsquared <emsquared@users.noreply.github.com>
2019-08-01 12:51:21 -0400

    Extend Book to write to disk.

--- Classes/Book.swift
+++ Classes/Book.swift
@@ -35,6 +35,7 @@
  *********************************************************************** */
 
 import Foundation
+import os.log
 
 ///
 /// `Book` represents a specific manga.
@@ -182,5 +183,147 @@ final class BookMutable
 	func copy() throws -> Book
 	{
 		return try Book(with: self)
+	}
+}
+
+/* ------------------------------------------------------ */
+
+public extension Book
+{
+	///
+	/// Format to write contents of `Book` as.
+	///
+	enum WriteFormat
+	{
+		///
+		/// JSON
+		///
+		case json
+
+		///
+		/// Binary property list
+		///
+		case propertyList
+	}
+
+	///
+	/// Write contents of `Book` to `url` as `format`.
+	///
+	/// - Parameter url: Location to write data to.
+	/// - Parameter format: Format to write data as.
+	/// 					Defaults to binary property list.
+	///
+	/// - Returns: `true` on success. `false` otherwise.
+	///
+	func writeTo(url: URL, as format: WriteFormat = .propertyList) -> Bool
+	{
+		let data: Data?
+
+		switch (format) {
+			case .json:
+				data = jsonData
+			case .propertyList:
+				data = propertyListData
+		}
+
+		if (data == nil) {
+			return false
+		}
+
+		do {
+			try data!.write(to: url)
+
+			return true
+		} catch let error {
+			os_log("Writing failed with error: %@",
+				   log: Logging.Subsystem.general, type: .error, error.localizedDescription)
+
+			return false
+		}
+	}
+
+	///
+	/// Write contents of `Book` to Desktop folder.
+	///
+	/// - Warning: This function is for debug purposes. There is
+	/// no gurantee of its behavior or whether it will always exist.
+	///
+	@discardableResult
+	func writeToDebugLocation(as format: WriteFormat = .propertyList) -> Bool
+	{
+		let debugLocation = URL(fileURLWithPath: "\(NSHomeDirectory())/Desktop/book.txt")
+
+		return writeTo(url: debugLocation, as: format)
+	}
+
+	///
+	/// JSON representation of `Book`.
+	///
+	var json: String?
+	{
+		guard let data = jsonData else {
+			return nil
+		}
+
+		return String(data: data, encoding: .utf8)
+	}
+
+	///
+	/// JSON representation of `Book`.
+	///
+	var jsonData: Data?
+	{
+		do {
+			let encoder = JSONEncoder()
+
+			let data = try encoder.encode(self)
+
+			return data
+		} catch let error {
+			os_log("Encoding failed with error: %@",
+				   log: Logging.Subsystem.general, type: .error, error.localizedDescription)
+
+			return nil
+		}
+	}
+
+	///
+	/// XML property list representation of `Book`.
+	///
+	var propertyList: String?
+	{
+		guard let data = propertyListData(as: .xml) else {
+			return nil
+		}
+
+		return String(data: data, encoding: .utf8)
+	}
+
+	///
+	/// Binary property list representation of `Book`.
+	///
+	var propertyListData: Data?
+	{
+		return propertyListData(as: .binary)
+	}
+
+	///
+	/// Property list representation of `Book` as `format`.
+	///
+	fileprivate func propertyListData(as format: PropertyListSerialization.PropertyListFormat) -> Data?
+	{
+		do {
+			let encoder = PropertyListEncoder()
+			encoder.outputFormat = format
+
+			let data = try encoder.encode(self)
+
+			return data
+		} catch let error {
+			os_log("Encoding failed with error: %@",
+				   log: Logging.Subsystem.general, type: .error, error.localizedDescription)
+
+			return nil
+		}
 	}
 }
